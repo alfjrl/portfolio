c783e4ab51587aefefcf819a667c804df9f7a7e2
Alfred Lin <>
2023-05-04 13:26:47 -0400

    update experience content

--- src/data/exp.js
+++ src/data/exp.js
@@ -8,6 +8,13 @@ export default function exp() {
     },
     {
       id: 2,
+      title: "Product Designer | Obran Cooperative",
+      description:
+        "Design and manage the Disability Summit website with a focus on accessibility. Develope new website using Next.js, NextUI(React UI Library), and Strapi.",
+      time: "2022 October - 2023 May",
+    },
+    {
+      id: 2,
       title: "Web Developer/Desinger | Including Disability Global Summit",
       description:
         "Design and manage the Disability Summit website with a focus on accessibility. Develope new website using Next.js, NextUI(React UI Library), and Strapi.",
@@ -15,28 +22,28 @@ export default function exp() {
     },
     {
       id: 3,
-      title: "Product Design Intern | Oort",
+      title: "Product Designer | Nakamoto & Turing Labs",
       description:
         "Desinged features and UI components for the cloud storage dashboard that aligns with the existing design system and user experience. Designed and built web3.0 cloud storage webiste homepage. ",
       time: "2022 Summer",
     },
     {
       id: 4,
-      title: "Software Product Manager | Hyena Inc.",
+      title: "Software Product Designer | Hyena Inc.",
       description:
         "Redesigned and optimized the system user flows and designed e-bike manufacture management systems, solving existing usability issues while making it efficient and scalable",
       time: "2020 September - 2021 August",
     },
     {
       id: 5,
-      title: "Product Manager | KNST Co., LTD.",
+      title: "Product Designer | KNST Co., LTD.",
       description:
         "Designed and modified the user flow, and delivered usability improvements and new features to the online postal service with the result of 400% order increases in 3 months.",
       time: "2020 April - 2020 September",
     },
     {
       id: 6,
-      title: "Management Specialist | Giant Bicycles",
+      title: "Product Designer | Giant Bicycles",
       description:
         "Coordinated, executed the implementation of SAP to replace the legacy ERP system, designing and integrating the internal workflow into SAP to streamline the global supply chain management between factories across continents.",
       time: "2018 July - 2019 August",
